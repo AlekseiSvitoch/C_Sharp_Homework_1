1d306f7022ade6d2fb57e03707e0adf3f896a0ff
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-22 17:20:56 +0600

    complete task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int div = number % 2;
+
+if (div == 0)
+    Console.Write("Введенное число является четным");
+
+else
+    Console.Write("Введенное число является нечетным");
