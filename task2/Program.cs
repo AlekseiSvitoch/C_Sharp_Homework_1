f933943ae9e3a7743e7420032e4401d38ad8cdec
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-22 14:23:13 +0600

    complete task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// task2
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.Write(numberA);
+}
+
+else
+{
+    Console.Write("max = ");
+    Console.Write(numberB);
+}
---
+++ task4/Program.cs
@@ -0,0 +1,6 @@
+﻿// task4
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
