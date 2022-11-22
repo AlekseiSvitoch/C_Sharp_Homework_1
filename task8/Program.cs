6064b74ff4271cd5fb5c4bdfeb716d2cbae88ac4
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-22 18:49:10 +0600

    complete task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = number / number;
+while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+    i++;
+}
